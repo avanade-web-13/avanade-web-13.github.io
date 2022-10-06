71bfbc3fd1c3042e635c87227917429f07930433
Marilia Santos Barbosa <m.santos.barbosa@avanade.com>
2022-10-06 15:30:42 -0300

    Deploy

--- assets/js/loja.js
+++ assets/js/loja.js
@@ -1,8 +1,28 @@
 const produto = document.querySelector('#title');
 
-const showData = async (json) => {
-        
+const showData = async (result) => {
+    for (const campo in result) {
+        if (document.querySelector('#' + campo)) {
+            document.querySelector('#' + campo).value = result[campo];
+        }
     }
+}
+
+// const showProducts = async(e) => {
+//     const options = {
+//         method: 'GET',
+//         mode: 'cors', 
+//         cache: 'default',
+//     };
+
+//     const itens = await fetch(`https://dummyjson.com/products`, options) 
+//     const json = await itens.json();
+
+//     for(i = 0; i < 10; i++) {
+//         console.log(json.products[i]);
+//     }
+// }
+
 
 produto.addEventListener("blur", async (e) => {
     
@@ -18,5 +38,7 @@ produto.addEventListener("blur", async (e) => {
     for(i = 0; i < 10; i++) {
         console.log(json.products[i]);
     }
+
+    showData(json);
 
 });
